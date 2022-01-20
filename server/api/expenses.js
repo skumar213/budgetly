faffe4801d0df006abfbfa5b856c38f620fd53ab
Sabi Kumar <skumar213@gmail.com>
2022-01-20 16:20:32 -0500

    added expenses get route

--- server/api/expenses.js
+++ server/api/expenses.js
@@ -5,5 +5,20 @@ const {
 const { requireToken } = require("./gateKeepingMiddleware");
 module.exports = router;
 
-
 //GET /expenses, gets all current expenses for user
+router.get("/", requireToken, async (req, res, next) => {
+  try {
+    const userExpenses = await User.findOne({
+      where: {
+        id: req.user.id,
+      },
+      include: {
+        model: Expense,
+      },
+    });
+
+    res.send(userExpenses);
+  } catch (error) {
+    next(error);
+  }
+});
