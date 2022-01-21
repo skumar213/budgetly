dd7e388fe92f5650ddbd6a932d9de7fc7bb144af
Sabi Kumar <skumar213@gmail.com>
2022-01-21 16:00:08 -0500

    working on expenses delete route, create done

--- client/store/expenses.js
+++ client/store/expenses.js
@@ -3,10 +3,14 @@ import { authenticateRequest } from "./gateKeepingMiddleware";
 //ACTION TYPES
 const SET_EXPENSES = "SET_EXPENSES";
 const UPDATE_EXPENSE = "UPDATE_EXPENSE";
+const CREATE_EXPENSE = "CREATE_EXPENSE"
+const DELETE_EXPENSE = "DELETE_EXPENSE";
 
 //ACTION CREATORS
 const setExpenses = expenses => ({ type: SET_EXPENSES, expenses });
 const updateExpense = expense => ({ type: UPDATE_EXPENSE, expense });
+const createExpense = expense => ({type: CREATE_EXPENSE, expense});
+const deleteExpense = expense => ({type: DELETE_EXPENSE, expense});
 
 //THUNK CREATORS
 export const _setExpenses = expenses => {
@@ -29,6 +33,28 @@ export const _updateExpenses = newExpInfo => async dispatch => {
   }
 };
 
+export const _createExpense = newExp => async dispatch => {
+  try {
+    const newExpense = await authenticateRequest('post', '/api/expenses', newExp);
+
+    dispatch(createExpense(newExpense))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const _deleteExpense = exp => async dispatch => {
+  try {
+    const deleteConfirmation = await authenticateRequest('delete', '/api/expense', exp)
+
+    if ()
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 //REDUCER
 export default function (state = [], action) {
   switch (action.type) {
@@ -38,6 +64,9 @@ export default function (state = [], action) {
       const updatedExpenses = state.filter(exp => exp.id !== action.expense.id);
       updatedExpenses.push(action.expense);
       return updatedExpenses;
+    case CREATE_EXPENSE:
+      const newExpenses = [...state, action.expense]
+      return newExpenses
     default:
       return state;
   }
