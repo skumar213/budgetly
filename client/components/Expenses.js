3dca8719ce59aa6d9ae64067d3048fef44c6add1
Sabi Kumar <skumar213@gmail.com>
2022-01-21 17:11:31 -0500

    basic layout for expense comp is done

--- client/components/Expenses.js
+++ client/components/Expenses.js
@@ -1,19 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import history from "../history";
-import { _getExpenses, _updateExpense, _createExpense, _deleteExpense} from "../store/expenses";
+import {
+  _getExpenses,
+  _updateExpense,
+  _createExpense,
+  _deleteExpense,
+} from "../store/expenses";
 
 const UserProfile = () => {
   const dispatch = useDispatch();
-  const allExpenses = useSelector(state => state.expenses);
+  const allExpenses = useSelector(state => state.expenses).expenses || [];
 
-  
+  useEffect(() => {
+    dispatch(_getExpenses());
+  }, []);
 
-
-
+  console.log(allExpenses);
 
   return (
-    <div>Expenses</div>
+    <div>
+      {allExpenses.map(exp => {
+        return (
+          <div>
+            <form>
+              <div>
+                <label htmlFor="merchant">
+                  <small>Merchant</small>
+                </label>
+                <input name="merchant" type="text" value={exp.merchant} />
+              </div>
+              <div>
+                <label htmlFor="amount">
+                  <small>Last Name</small>
+                </label>
+                <input name="amount" type="text" value={exp.amount} />
+              </div>
+              <div>
+                <label htmlFor="dueDate">
+                  <small>dueDate</small>
+                </label>
+                <input name="dueDate" type="text" value={exp.dueDate} />
+              </div>
+              <div>
+                <label htmlFor="paidDate">
+                  <small>paidDate</small>
+                </label>
+                <input name="paidDate" type="paidDate" value={exp.paidDate} />
+              </div>
+              <div>
+                <label htmlFor="isRepeat">
+                  <small>Repeat</small>
+                </label>
+                <input
+                  name="isRepeat"
+                  type="number"
+                  value={exp.isRepeat}
+                />
+              </div>
+              <div>
+                <button type="submit">Update</button>
+              </div>
+            </form>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
