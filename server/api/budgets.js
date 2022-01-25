75670d3bc7da8b59b5a68ee9f57cda7204a14ffc
Sabi Kumar <skumar213@gmail.com>
2022-01-25 17:17:07 -0500

    made post route for budgets

--- server/api/budgets.js
+++ server/api/budgets.js
@@ -5,31 +5,29 @@ const {
 const { requireToken } = require("./gateKeepingMiddleware");
 module.exports = router;
 
-
 //GET /budget, gets all current budgets for user
-router.get('/', requireToken, async (req, res, next) => {
+router.get("/", requireToken, async (req, res, next) => {
   try {
     const userBudgets = await User.findOne({
       where: {
-        id: req.user.id
+        id: req.user.id,
       },
       include: {
         model: Budget,
         include: {
-          model: Category
-        }
-      }
-    })
+          model: Category,
+        },
+      },
+    });
 
-    res.send(userBudgets)
+    res.send(userBudgets);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
-
+});
 
 //PUT /budgets, updates a single budget for a user
-router.put('/', requireToken, async (req, res, next) => {
+router.put("/", requireToken, async (req, res, next) => {
   try {
     const userBudget = await Budget.findOne({
       where: {
@@ -46,7 +44,7 @@ router.put('/', requireToken, async (req, res, next) => {
           model: Category,
         },
       ],
-    })
+    });
 
     if (req.body.category) {
       const expCatName = req.body.category;
@@ -78,10 +76,55 @@ router.put('/', requireToken, async (req, res, next) => {
           model: Category,
         },
       ],
-    })
+    });
 
-    res.send(updatedBudget).status(202)
+    res.send(updatedBudget).status(202);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
+
+router.post("/", requireToken, async (req, res, next) => {
+  try {
+    const expCatName = req.body.category;
+    delete req.body.category;
+
+    const newBudget = await Budget.create(req.body);
+
+
+    const category = await Category.findOne({
+      where: {
+        name: expCatName,
+      },
+    });
+
+    const user = await User.findByPk(req.user.id);
+    console.log(user)
+
+    await user.addBudget(newBudget);
+
+    await newBudget.setCategory(category)
+
+    const budgetWithCategory = await Budget.findOne({
+      where: {
+        id: newBudget.id,
+      },
+      include: [
+        {
+          model: User,
+          where: {
+            id: req.user.id,
+          },
+        },
+        {
+          model: Category,
+        },
+      ],
+    });
+
+
+    res.send(budgetWithCategory).status(201);
+  } catch (error) {
+    next(error);
+  }
+});
