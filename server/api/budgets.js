cd3880c0814d10d2a4a606aac175e9382d72776d
Sabi Kumar <skumar213@gmail.com>
2022-01-25 17:07:49 -0500

    made budgets put route

--- server/api/budgets.js
+++ server/api/budgets.js
@@ -26,3 +26,62 @@ router.get('/', requireToken, async (req, res, next) => {
     next(error)
   }
 })
+
+
+//PUT /budgets, updates a single budget for a user
+router.put('/', requireToken, async (req, res, next) => {
+  try {
+    const userBudget = await Budget.findOne({
+      where: {
+        id: req.body.id,
+      },
+      include: [
+        {
+          model: User,
+          where: {
+            id: req.user.id,
+          },
+        },
+        {
+          model: Category,
+        },
+      ],
+    })
+
+    if (req.body.category) {
+      const expCatName = req.body.category;
+      delete req.body.category;
+
+      const category = await Category.findOne({
+        where: {
+          name: expCatName,
+        },
+      });
+
+      await userBudget.setCategory(category);
+    }
+
+    await userBudget.update(req.body);
+
+    const updatedBudget = await Budget.findOne({
+      where: {
+        id: req.body.id,
+      },
+      include: [
+        {
+          model: User,
+          where: {
+            id: req.user.id,
+          },
+        },
+        {
+          model: Category,
+        },
+      ],
+    })
+
+    res.send(updatedBudget).status(202)
+  } catch (error) {
+    next(error)
+  }
+})
