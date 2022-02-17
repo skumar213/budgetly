603a6dbfa5fa16eee1ffa0739134237b814e96b6
Sabi Kumar <skumar213@gmail.com>
2022-02-17 17:31:24 -0500

    added seed update file

---
+++ script/seedUpdate.js
@@ -0,0 +1,145 @@
+"use strict";
+
+const {
+  db,
+  models: { User, Category, Expense, Budget, Investment, MonthlyIncome },
+} = require("../server/db");
+
+/**
+ * seed - this function clears the database, updates tables to
+ *      match the models, and populates the database.
+ */
+async function seed() {
+  await db.sync({ force: false }); // will not clear db
+  console.log("db synced!");
+
+  const todayDate = new Date();
+
+  const dueDate = new Date(`${todayDate.getMonth() + 1}/25/${todayDate.getFullYear()}`);
+  const paidDate = new Date(`${todayDate.getMonth() + 1}/20/${todayDate.getFullYear()}`);
+  const createdAt = new Date(`${todayDate.getMonth() + 1}/1/${todayDate.getFullYear()}`);
+
+  // Creating current month Expenses
+  const expenses = await Promise.all([
+    Expense.create({
+      merchant: "apt rent",
+      amount: 1500,
+      dueDate,
+      paidDate,
+    }),
+    Expense.create({
+      merchant: "peco",
+      amount: 200,
+      dueDate,
+      paidDate,
+    }),
+    Expense.create({
+      merchant: "whole Foods",
+      amount: 600,
+      dueDate,
+      paidDate,
+    }),
+    Expense.create({
+      merchant: "Watching a movie with friends",
+      amount: 50,
+      dueDate,
+      paidDate,
+    }),
+    Expense.create({
+      merchant: "Savings for the month",
+      amount: 500,
+      dueDate,
+      paidDate,
+    }),
+    Expense.create({
+      merchant: "Gym Membership",
+      amount: 30,
+      dueDate,
+      paidDate,
+    }),
+  ]);
+
+  const outstandingExpenses = await Promise.all([
+    Expense.create({
+      merchant: "Cell phone",
+      amount: 100,
+      dueDate,
+      repeat: true,
+    }),
+    Expense.create({
+      merchant: "Costco",
+      amount: 80,
+      dueDate,
+    }),
+  ]);
+
+  // Creating current months budgets
+  const budgets = await Promise.all([
+    Budget.create({ amount: 1500, createdAt }),
+    Budget.create({ amount: 110, createdAt }),
+    Budget.create({ amount: 750, createdAt }),
+    Budget.create({ amount: 150, createdAt }),
+    Budget.create({ amount: 500, createdAt }),
+    Budget.create({ amount: 100, createdAt }),
+  ]);
+
+  const categories = await Category.findAll({});
+
+
+
+  // Assigning user to expenses/investments and each expense to a category
+  const user = await User.findOne({
+    where: {
+      email: "mike@gmail.com",
+    },
+  });
+
+  //adding paid expenses for current month to user
+  for (let i = 0; i < expenses.length; i++) {
+    //setting category for expenses
+    await expenses[i].setCategory(categories[i]);
+
+    //setting category for budgets
+    await budgets[i].setCategory(categories[i]);
+
+    //adding expenses and budgets to user
+    await user.addExpense(expenses[i]);
+    await user.addBudget(budgets[i]);
+  }
+
+  //adding unpaid expesnes for current month to user
+  for (let i = 0; i < outstandingExpenses.length; i++) {
+    await outstandingExpenses[i].setCategory(categories[i + 1]);
+    await user.addExpense(outstandingExpenses[i]);
+  }
+}
+
+
+
+/*
+ We've separated the `seed` function from the `runSeed` function.
+ This way we can isolate the error handling and exit trapping.
+ The `seed` function is concerned only with modifying the database.
+*/
+async function runSeed() {
+  console.log("seeding...");
+  try {
+    await seed();
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  } finally {
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
+  }
+}
+
+/*
+  Execute the `seed` function, IF we ran this module directly (`node seed`).
+  `Async` functions always return a promise, so we can use `catch` to handle
+  any errors that might occur inside of `seed`.
+*/
+if (module === require.main) {
+  runSeed();
+}
