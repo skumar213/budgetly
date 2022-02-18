347c5411a14a6da9a54867e4b194836fae6ab514
Sabi Kumar <skumar213@gmail.com>
2022-02-18 16:26:56 -0500

    starting work on user profile styling

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -239,7 +239,7 @@ export const Signup = () => {
                     </div>
                     <div className="text-center">
                       <Link to="" onClick={handleSeededDemo}>
-                      In a rush? View seeded demo without an account!
+                        In a rush? View seeded demo without an account!
                       </Link>
                     </div>
 
